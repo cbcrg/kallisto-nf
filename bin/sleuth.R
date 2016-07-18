997b7c1e234c240c4a894391d75c2c8f83f8b021
Evan Floden <evanfloden@gmail.com>
2016-07-18 13:58:17 +0200

    Update sleuth.R to match new Sleuth introduction

--- bin/sleuth.R
+++ bin/sleuth.R
@@ -16,18 +16,25 @@ s2c <- s2c[order(s2c$condition), ]
 
 print(s2c)
 
-mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host="www.ensembl.org")
+#mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host="www.ensembl.org")
 
-t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id", "external_gene_name"), mart = mart)
-t2g <- dplyr::rename(t2g, target_id = ensembl_transcript_id, ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
+#t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id", "external_gene_name"), mart = mart)
+#t2g <- dplyr::rename(t2g, target_id = ensembl_transcript_id, ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
 
-so <- sleuth_prep(s2c, ~ condition, target_mapping = t2g)
+# Load the kallisto processed data into the object
+so <- sleuth_prep(s2c, ~ condition) #, target_mapping = t2g)
+
+# Estimate parameters for the sleuth response error measurement (full) model 
 so <- sleuth_fit(so)
 
-so <- sleuth_wt(so, 'conditionscramble')
+# estimate parameters for the sleuth reduced mode
+so <- sleuth_fit(so, ~1, 'reduced')
 
-gene_table <- sleuth_gene_table(so, test = "conditionscramble", test_type = "wt")
+# Perform test
+so <- sleuth_lrt(so, 'reduced', 'full')
 
-write.table(gene_table, paste("gene_table_results.txt"), sep="\t")
+#gene_table <- sleuth_gene_table(so, test = "conditionscramble", test_type = "wt")
+
+#write.table(gene_table, paste("gene_table_results.txt"), sep="\t")
 
 save(so, file=paste("sleuth_object.so"))
